e7c32b925ee17b368e08d9cf1e7b24091c7e9abb
Jaymes Winger <rathmere@gmail.com>
2018-02-25 11:02:24 -0800

    Working example site!

---
+++ src/js/views/example/index.jsx
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { actions as exampleActions } from '../../redux/modules/example';
+import { exampleSelector } from '../../redux/selectors/exampleSelector';
+import { Example, ExampleWithError } from '../../common/components/Example';
+import { ErrorBoundary } from '../../common/components/Utilities';
+
+require('../../../style/index.css');
+
+const mapStateToProps = state => ({
+  example: exampleSelector(state),
+});
+
+const mapDispatchToProps = {
+  ...exampleActions,
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+class ExampleView extends Component {
+  componentDidMount() {
+    this.props.getAwesomeCode();
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Example {...this.props} />
+        <ErrorBoundary>
+          <ExampleWithError {...this.props} />
+        </ErrorBoundary>
+      </Fragment>
+    )
+  }
+}
+
+ExampleView.propTypes = {
+  example: PropTypes.object.isRequired,
+}
+
+export default ExampleView;
